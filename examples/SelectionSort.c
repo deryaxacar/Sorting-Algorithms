e982b70f9289ae35b4e016cdd17162be44680060
Derya Acar <118841541+deryaxacar@users.noreply.github.com>
2023-11-25 18:10:14 +0300

    Update SelectionSort.c

--- examples/SelectionSort.c
+++ examples/SelectionSort.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
-void selectionSort(int arr[], int n) {
+void selectionSort(int arr[], int n) 
+{
     int i, j, min, tmp;
     
-    for (i = 0; i < n-1; i++) {
+    for (i = 0; i < n-1; i++) 
+    {
         // Minimum elemanın indeksini bulma
         min = i;
-        for (j = i+1; j < n; j++) {
-            if (arr[j] < arr[min]) {
+        for (j = i+1; j < n; j++) 
+        {
+            if (arr[j] < arr[min]) 
                 min = j;
-            }
         }
         
         // Minimum elemanı bulunan yerle değiştirme
@@ -20,18 +22,21 @@ void selectionSort(int arr[], int n) {
 }
 
 // Örnek kullanım
-int main() {
+int main() 
+{
     int Array[] = {64, 25, 12, 22, 11};
     int n = sizeof(Array) / sizeof(Array[0]);
     
     printf("önce: ");
-     for (int i = 0; i < n; i++) {
+     for (int i = 0; i < n; i++) 
+    {
         printf("%d ", Array[i]);
     }
     printf("\n");
     selectionSort(Array, n);
     printf("Sonra: ");
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) 
+    {
         printf("%d ", Array[i]);
     }
     
